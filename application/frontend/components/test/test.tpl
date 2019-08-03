51fcca2fa9af549d7d3e5f4f5a54ef0b2030e94d
olezhikz <boxmilo@gmail.com>
2019-08-03 18:18:51 +0600

    asset

--- application/frontend/components/test/test.tpl
+++
@@ -1,9 +0,0 @@
-{**
- * Список плагинов
- *
- * @param array $plugins Список плагинов
- *}
-
-{component_define_params params=[ 'testVar' ]}
-
-test_component {$testVar}
--- web/assets/bootstrap/js/component_media1library_init.js
+++
@@ -1,7 +0,0 @@
-ls.hook.add('ls_template_init_end', function(){ 
-    $('[data-library]').mediaLibrary();
-    $('#modalInsert').mediaModalInsert();
-    $('[data-media-field]').mediaField();
-    $('[data-cropper]').mediaCropper();
-});
-
